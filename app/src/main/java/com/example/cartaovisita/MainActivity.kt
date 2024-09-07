85252eaacbd67e2c7c0e27d4d8a1c43cf0a2405f
Paulo-Goes <paulo.2021104820@unicap.br>
2024-09-07 13:58:21 -0300

    Cor do plano de fundo Foto de fundo Foto minha

--- app/src/main/java/com/example/cartaovisita/MainActivity.kt
+++ app/src/main/java/com/example/cartaovisita/MainActivity.kt
@@ -7,14 +7,21 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -30,14 +37,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CartaoVisitaTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color.Gray
                 ) {
-                    CartaoImage(
-                        stringResource(R.string.nome),
-                        stringResource(R.string.profissao)
+                    CartaoVisita(
+                        nome = stringResource(R.string.nome),
+                        profissao = stringResource(R.string.profissao),
+                        actualProfissao = stringResource(R.string.actualProfissao)
                     )
                 }
             }
@@ -46,57 +53,76 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CartaoText(nome: String, profissao: String, modifier: Modifier = Modifier) {
+fun CartaoVisita(
+    nome: String,
+    profissao: String,
+    actualProfissao: String,
+    modifier: Modifier = Modifier
+) {
+    Box(modifier) {
+        Image(
+            painter = painterResource(R.drawable.androidlogo),
+            contentDescription = null,
+            modifier = Modifier.align(Alignment.Center),
+            alpha = 0.7f
+        )
+
+    }
     Column(
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(16.dp),
     ) {
+        Image(
+            painter = painterResource(R.drawable.fotoeu),
+            contentDescription = null,
+            modifier = Modifier
+                .size(250.dp)
+                .clip(CircleShape)
+        )
         Text(
             text = nome,
-            fontSize = 100.sp,
+            fontSize = 60.sp,
+            color = Color(0xFF3ddc84),
             lineHeight = 116.sp,
             textAlign = TextAlign.Center,
             modifier = Modifier.padding(top = 16.dp)
         )
         Text(
             text = profissao,
+            fontSize = 50.sp,
+            color = Color(0xFF3ddc84),
+            modifier = Modifier
+                .padding(top = 60.dp)
+        )
+        Text(
+            text = actualProfissao,
             fontSize = 36.sp,
             modifier = Modifier
                 .padding(top = 16.dp)
-                .padding(end = 16.dp)
-                .align(alignment = Alignment.End)
-
         )
     }
-}
+    Column() {
+        Row() {
 
-@Composable
-fun CartaoImage(nome: String, profissao: String, modifier: Modifier = Modifier) {
-    val image = painterResource(R.drawable.linuxpenguin)
-    Box(modifier) {
-        Image(
-            painter = painterResource(id = R.drawable.linuxpenguin),
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            alpha = 0.5F
-        )
-        CartaoText(
-            nome = nome,
-            profissao = profissao,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(8.dp)
-        )
+        }
+        Row() {
+
+        }
     }
 }
 
 @Preview(showBackground = false)
 @Composable
 fun CartaoPreview() {
-    CartaoVisitaTheme {
-        CartaoImage(
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color.Gray
+    ) {
+        CartaoVisita(
             nome = stringResource(R.string.nome),
-            profissao = stringResource(R.string.profissao)
+            profissao = stringResource(R.string.profissao),
+            actualProfissao = stringResource(R.string.actualProfissao)
         )
     }
 }
